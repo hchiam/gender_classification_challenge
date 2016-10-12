7b18df3df9f01cebe72af6e9c1facb8d48a905a1
hchiam <hchiam@gmail.com>
2016-10-11 22:22:10 -0400

    add scoring and printing

--- demo.py
+++ demo.py
@@ -1,18 +1,19 @@
 # challenge instructions: https://github.com/llSourcell/gender_classification_challenge
-# get classifiers: http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
-# get scores: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
 
+# classifiers: http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
 from sklearn import tree
-
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
+# accuracy scoring: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
 from sklearn.metrics import accuracy_score
+
+
 
 clf = tree.DecisionTreeClassifier()
 
-## CHALLENGE - create 3 more classifiers...
+# CHALLENGE - create 3 more classifiers...
 # get classifiers: http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
 
 #1
@@ -29,24 +30,48 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
 
 
-#CHALLENGE - ...and train them on our data
+
+# CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
-prediction = clf.predict([[190, 70, 43]])
+prediction = clf.predict(X) # prediction = clf.predict([[190, 70, 43]])
 
 #1
 clf1 = clf1.fit(X, Y)
-prediction1 = clf1.predict([[190, 70, 43]])
+prediction1 = clf1.predict(X)
 #2
 clf2 = clf2.fit(X, Y)
-prediction2 = clf2.predict([[190, 70, 43]])
+prediction2 = clf2.predict(X)
 #3
 clf3 = clf3.fit(X, Y)
-prediction3 = clf3.predict([[190, 70, 43]])
+prediction3 = clf3.predict(X)
 
-#CHALLENGE compare their results and print the best one!
+
+
+# CHALLENGE compare their results and print the best one!
 # get scores: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
 
-print prediction
-print prediction1
-print prediction2
-print prediction3
+#print prediction
+#print prediction1
+#print prediction2
+#print prediction3
+
+Y_true = Y
+
+score = accuracy_score(Y_true, prediction)
+score1 = accuracy_score(Y_true, prediction1)
+score2 = accuracy_score(Y_true, prediction2)
+score3 = accuracy_score(Y_true, prediction3)
+#print score
+#print score1
+#print score2
+#print score3
+
+best_score = max(score,score1,score2,score3)
+print("Best score: \n  " + str(best_score))
+
+classifier_names = {'tree': score, 'GaussianNB': score1, 'KNeighborsClassifier': score2, 'SVC': score3}
+#print classifier_names
+
+best_scoring_classifiers = [name for name in classifier_names if classifier_names[name] == best_score]
+print("Best scorer(s): ")
+for best_scorers in best_scoring_classifiers: print("  "+best_scorers)
